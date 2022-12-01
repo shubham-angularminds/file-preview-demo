71a3338074ac57a756d28e7458cac12c463b4971
shubham-am <shubham.patil@angularminds.com>
2022-12-01 18:17:36 +0530

    added top toolbar

--- src/components/modal.jsx
+++ src/components/modal.jsx
@@ -1,11 +1,26 @@
 import React from "react";
 import styles from "./Modal.module.css";
-import { RiCloseLine } from "react-icons/ri";
+import { RiCloseLine, RiFileChartLine } from "react-icons/ri";
 
 const Modal = ({ setIsOpen }) => {
   return (
     <>
-      <div className={styles.darkBG} onClick={() => setIsOpen(false)} />
+      <div className={styles.darkBG} onClick={() => setIsOpen(false)}>
+        <div className={styles.toolbarContainer}>
+          <div className={styles.toolbar}>
+            <div className={styles.toolbarLeft}>
+              <span className={styles.FileIcon}>
+                <RiFileChartLine style={{ color: "#fff", fontSize: "20px" }} />
+              </span>
+
+              <span className={styles.topFileName}>Sales-dashboard.png</span>
+            </div>
+            <div className={styles.toolbarRight}>
+              <div className={styles.rightButtons}></div>
+            </div>
+          </div>
+        </div>
+      </div>
       <div className={styles.centered}>
         <div className={styles.modal}>
           <div className={styles.modalHeader}>
