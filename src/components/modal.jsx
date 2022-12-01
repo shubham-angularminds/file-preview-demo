9a06b336c603daf79512ee573ff5f9c42f7ec6d3
shubham-am <shubham.patil@angularminds.com>
2022-12-01 17:05:01 +0530

    increase the size of modal

--- src/components/modal.jsx
+++ src/components/modal.jsx
@@ -9,15 +9,12 @@ const Modal = ({ setIsOpen }) => {
       <div className={styles.centered}>
         <div className={styles.modal}>
           <div className={styles.modalHeader}>
-            <h5 className={styles.heading}>Dialog</h5>
+            <h5 className={styles.heading}>Modal</h5>
           </div>
           <button className={styles.closeBtn} onClick={() => setIsOpen(false)}>
             <RiCloseLine style={{ marginBottom: "-3px" }} />
           </button>
-          <div className={styles.modalContent}>
-            Are you sure you want to delete the item?
-          </div>
-          <div className={styles.modalActions}>
+          {/* <div className={styles.modalActions}>
             <div className={styles.actionsContainer}>
               <button className={styles.deleteBtn} onClick={() => setIsOpen(false)}>
                 Delete
@@ -29,7 +26,7 @@ const Modal = ({ setIsOpen }) => {
                 Cancel
               </button>
             </div>
-          </div>
+          </div> */}
         </div>
       </div>
     </>
