f42759b8a008624b678715c9f62b0d1c0e6528a4
shubham-am <shubham.patil@angularminds.com>
2022-12-01 16:36:49 +0530

    custom modal implementation

--- src/App.js
+++ src/App.js
@@ -1,64 +1,18 @@
 import React, { useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import SamplePdf from './sample.pdf';
-import { Document, Page } from 'react-pdf/dist/esm/entry.webpack5';
-import Modal from 'react-modal';
-import FileViewer from "react-file-viewer";
-
-
-Modal.setAppElement('#root');
-
-
+import styles from "./App.module.css";
+import Modal from "./components/modal";
 
 function App() {
-  const [modalIsOpen, setIsOpen] = React.useState(false);
-  const [numPages, setNumPages] = useState(null);
-  const [pageNumber, setPageNumber] = useState(1);
-
-  let subtitle;
-
-  function openModal() {
-    setIsOpen(true);
-  }
-
-  function afterOpenModal() {
-    // references are now sync'd and can be accessed.
-    subtitle.style.color = '#f00';
-  }
-
-  function closeModal() {
-    setIsOpen(false);
-  }
-
-  function onDocumentLoadSuccess({ numPages }) {
-    setNumPages(numPages);
-  }
-
-  const file = "./sample.pdf";
-  const type = "pdf";
-  const onError = e => {
-  console.log(e, "error in file-viewer");
-};
-
+const [isOpen, setIsOpen] = useState(false);
 
   return (
     <>
      <div>
-  
-     </div>
-     <div>
-      <button onClick={openModal}>Open Modal</button>
-      <Modal
-        isOpen={modalIsOpen}
-        onAfterOpen={afterOpenModal}
-        onRequestClose={closeModal}
-        contentLabel="Example Modal"
-      >
-              <FileViewer fileType={type} filePath={file} onError={onError}/>
-
-      </Modal>
-
+     <button className={styles.primaryBtn} onClick={() => setIsOpen(true)}>
+        Open Modal
+      </button>
+      {isOpen && <Modal setIsOpen={setIsOpen} />}
     </div>
     </>
   );
