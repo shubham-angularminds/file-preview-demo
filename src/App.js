a9f75256f6f01c83d56c5e87beeb43fc41779bfe
shubham-am <shubham.patil@angularminds.com>
2022-12-02 17:38:43 +0530

    pdf integration

--- src/App.js
+++ src/App.js
@@ -1,19 +1,55 @@
-import React, { useState } from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 import styles from "./App.module.css";
 import Modal from "./components/modal";
+import SampleImage from "./sample.jpg";
+import SamplePdf from "./sample.pdf";
 
 function App() {
-const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+  const files = [
+    {
+      file: SampleImage,
+      fileType: "image",
+      fileName: "sample-image1.jpg",
+    },
+    {
+      file: SamplePdf,
+      fileType: "pdf",
+      fileName: "sample-pdf.pdf",
+    },
+  ];
+
+  const [currentFileIndex, setCurrentFileIndex] = useState(0);
+
+  const handleNextFile = () => {
+    setCurrentFileIndex(currentFileIndex + 1);
+  };
+
+  const handleBeforeFile = () => {
+    setCurrentFileIndex(currentFileIndex - 1);
+  };
+
+  const fileLength = files.length;
 
   return (
     <>
-     <div>
-     <button className={styles.primaryBtn} onClick={() => setIsOpen(true)}>
-        Open Modal
-      </button>
-      {isOpen && <Modal setIsOpen={setIsOpen} />}
-    </div>
+      <div>
+        <button className={styles.primaryBtn} onClick={() => setIsOpen(true)}>
+          Open Modal
+        </button>
+        {isOpen && (
+          <Modal
+            setIsOpen={setIsOpen}
+            file={files[currentFileIndex]}
+            fileLength={fileLength}
+            currentFileIndex={currentFileIndex}
+            handleNextFile={handleNextFile}
+            handleBeforeFile={handleBeforeFile}
+            setCurrentFileIndex={setCurrentFileIndex}
+          />
+        )}
+      </div>
     </>
   );
 }
--- src/components/modal.jsx
+++ src/components/modal.jsx
@@ -1,13 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Modal.module.css";
 import { RiCloseLine, RiFileChartLine } from "react-icons/ri";
 import { FiDownload, FiMaximize } from "react-icons/fi";
-import { MdOutlineClose, MdCached, MdZoomIn, MdZoomOut } from "react-icons/md";
+import {
+  MdCached,
+  MdZoomIn,
+  MdZoomOut,
+  MdOutlineNavigateNext,
+  MdOutlineNavigateBefore,
+} from "react-icons/md";
 import classNames from "classnames";
 import { FullScreen, useFullScreenHandle } from "react-full-screen";
+import { Document, Page } from "react-pdf/dist/esm/entry.webpack5";
+import SamplePdf from "../sample.pdf";
 
-const Modal = ({ setIsOpen }) => {
+const Modal = ({
+  setIsOpen,
+  file,
+  handleNextFile,
+  handleBeforeFile,
+  fileLength,
+  currentFileIndex,
+  setCurrentFileIndex,
+}) => {
   const handle = useFullScreenHandle();
+
+  const [numPages, setNumPages] = useState(null);
+  const [pageNumber, setPageNumber] = useState(1);
+
+  function onDocumentLoadSuccess({ numPages }) {
+    setNumPages(numPages);
+  }
+
 
   return (
     <>
@@ -18,21 +42,25 @@ const Modal = ({ setIsOpen }) => {
               <span className={styles.FileIcon}>
                 <RiFileChartLine style={{ color: "#fff", fontSize: "20px" }} />
               </span>
-
-              <span className={styles.topFileName}>Sales-dashboard.png</span>
+              Reset
+              {}
+              <span className={styles.topFileName}>{file.fileName}</span>
             </div>
             <div className={styles.toolbarRight}>
               <span className={styles.rightBtns} onClick={handle.enter}>
                 <FiMaximize style={{ color: "#fff", fontSize: "20px" }} />
               </span>
               <span className={styles.rightBtns}>
-                <a href='./sample.jpg' download='my_file.jpg'>
+                <a href="./sample.jpg" download="my_file.jpg">
                   <FiDownload style={{ color: "#fff", fontSize: "20px" }} />
                 </a>
               </span>
               <span
                 className={styles.rightBtns}
-                onClick={() => setIsOpen(false)}
+                onClick={() => {
+                  setIsOpen(false);
+                  setCurrentFileIndex(0);
+                }}
               >
                 {" "}
                 <RiCloseLine style={{ color: "#fff", fontSize: "20px" }} />
@@ -42,13 +70,44 @@ const Modal = ({ setIsOpen }) => {
         </div>
       </div>
       <div className={styles.centered}>
-        <div className={styles.modal}>
+        <div className={file.fileType === 'image' ? styles.imageModal: styles.pdfModal}>
           <div className={styles.modalContent}>
             <FullScreen handle={handle}>
-              <img src="./sample.jpg"></img>
+              {file?.fileType === "image" ? (
+                <img src="./sample.jpg"></img>
+              ) : (
+                <div className={styles.pdfRender}>
+                  <Document
+                    file={file.file}
+                    onLoadSuccess={onDocumentLoadSuccess}
+                  >
+                    <Page pageNumber={pageNumber} />
+                  </Document>
+                  <p style={{ color: "#fff" }}>
+                    Page {pageNumber} of {numPages}
+                  </p>
+                </div>
+              )}
             </FullScreen>
           </div>
+          <div className={styles.sliderButtons}>
+            {currentFileIndex !== 0 && (
+              <MdOutlineNavigateBefore
+                className={styles.navigatePrevious}
+                style={{ color: "#fff", fontSize: "30px" }}
+                onClick={handleBeforeFile}
+              />
+            )}
+            {currentFileIndex !== fileLength - 1 && (
+              <MdOutlineNavigateNext
+                className={styles.navigateNext}
+                style={{ color: "#fff", fontSize: "30px" }}
+                onClick={handleNextFile}
+              />
+            )}
+          </div>
         </div>
+
         <div className={classNames(styles.lowerButtonsSection)}>
           <span className={styles.lowerBtns}>
             <MdCached style={{ color: "#fff", fontSize: "20px" }} />
