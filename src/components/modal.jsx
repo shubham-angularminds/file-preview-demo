7ae49faade350f053ea2af37289b95b29b378209
shubham-am <shubham.patil@angularminds.com>
2022-12-02 14:52:57 +0530

    full screen and download integration

--- src/components/modal.jsx
+++ src/components/modal.jsx
@@ -2,12 +2,16 @@ import React from "react";
 import styles from "./Modal.module.css";
 import { RiCloseLine, RiFileChartLine } from "react-icons/ri";
 import { FiDownload, FiMaximize } from "react-icons/fi";
-import { MdOutlineClose } from "react-icons/md";
+import { MdOutlineClose, MdCached, MdZoomIn, MdZoomOut } from "react-icons/md";
+import classNames from "classnames";
+import { FullScreen, useFullScreenHandle } from "react-full-screen";
 
 const Modal = ({ setIsOpen }) => {
+  const handle = useFullScreenHandle();
+
   return (
     <>
-      <div className={styles.darkBG} onClick={() => setIsOpen(false)}>
+      <div className={styles.darkBG}>
         <div className={styles.toolbarContainer}>
           <div className={styles.toolbar}>
             <div className={styles.toolbarLeft}>
@@ -18,43 +22,50 @@ const Modal = ({ setIsOpen }) => {
               <span className={styles.topFileName}>Sales-dashboard.png</span>
             </div>
             <div className={styles.toolbarRight}>
-              <div className={styles.rightButtons}>
-                <span>
-                  <FiMaximize style={{ color: "#fff", fontSize: "20px" }} />
-                </span>
-                <span>
+              <span className={styles.rightBtns} onClick={handle.enter}>
+                <FiMaximize style={{ color: "#fff", fontSize: "20px" }} />
+              </span>
+              <span className={styles.rightBtns}>
+                <a href='./sample.jpg' download='my_file.jpg'>
                   <FiDownload style={{ color: "#fff", fontSize: "20px" }} />
-                </span>{" "}
-                |
-                <span>
-                  <MdOutlineClose style={{ color: "#fff", fontSize: "20px" }} />
-                </span>
-              </div>
+                </a>
+              </span>
+              <span
+                className={styles.rightBtns}
+                onClick={() => setIsOpen(false)}
+              >
+                {" "}
+                <RiCloseLine style={{ color: "#fff", fontSize: "20px" }} />
+              </span>
             </div>
           </div>
         </div>
       </div>
       <div className={styles.centered}>
         <div className={styles.modal}>
-          <div className={styles.modalHeader}>
-            <h5 className={styles.heading}>Modal</h5>
+          <div className={styles.modalContent}>
+            <FullScreen handle={handle}>
+              <img src="./sample.jpg"></img>
+            </FullScreen>
           </div>
-          <button className={styles.closeBtn} onClick={() => setIsOpen(false)}>
-            <RiCloseLine style={{ marginBottom: "-3px" }} />
-          </button>
-          {/* <div className={styles.modalActions}>
-            <div className={styles.actionsContainer}>
-              <button className={styles.deleteBtn} onClick={() => setIsOpen(false)}>
-                Delete
-              </button>
-              <button
-                className={styles.cancelBtn}
-                onClick={() => setIsOpen(false)}
-              >
-                Cancel
-              </button>
-            </div>
-          </div> */}
+        </div>
+        <div className={classNames(styles.lowerButtonsSection)}>
+          <span className={styles.lowerBtns}>
+            <MdCached style={{ color: "#fff", fontSize: "20px" }} />
+          </span>
+          <span className={styles.lowerBtns}>
+            {" "}
+            <MdZoomOut style={{ color: "#fff", fontSize: "20px" }} />
+          </span>
+          <span className={styles.lowerBtns}>
+            <MdZoomIn style={{ color: "#fff", fontSize: "20px" }} />
+          </span>
+          <span
+            className={styles.lowerBtns}
+            style={{ color: "#fff", fontSize: "12px" }}
+          >
+            Reset
+          </span>
         </div>
       </div>
     </>
@@ -62,3 +73,14 @@ const Modal = ({ setIsOpen }) => {
 };
 
 export default Modal;
+
+{
+  /* <button className={styles.closeBtn} onClick={() => setIsOpen(false)}>
+            <RiCloseLine style={{ marginBottom: "-3px" }} />
+          </button> */
+}
+{
+  /* <div className={styles.modalHeader}>
+            <h5 className={styles.heading}>Modal</h5>
+          </div> */
+}
